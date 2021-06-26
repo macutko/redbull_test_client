084ba29f4c9615dbd2ff7cef1b24047eab943b70
macutko <matusgallik008@gmail.com>
2021-06-26 20:40:35 +0200

    chore: ssl

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://domacanas.hopto.org:11145",
+    baseURL: "https://domacanas.hopto.org:11145",
     withCredentials: true
 });
 
